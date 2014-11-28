7c513508913b1d07634ea66bfd5b4cd739139704
andrerpbts <andrerpbts@gmail.com>
2014-11-27 23:36:54 -0200

    Login encrypting keys

--- lib/steam_club_api/resources/login_resource.rb
+++ lib/steam_club_api/resources/login_resource.rb
@@ -1,21 +1,61 @@
 module SteamClubAPI
   class LoginResource < Resource
-    attr_reader :user_auth_url
+    attr_reader :user_auth_url, :session_key, :query, :certificate_file
 
     def initialize(options = {})
       super
       @user_auth_url = options.fetch :user_auth_url, default_user_auth_url
+      @certificate_file = options.fetch :certificate_file, default_certificate_file
+      @session_key = OpenSSL::Random.random_bytes(32)
+      @query = { sessionkey: crypted_session_key }
     end
 
-    def self.login(options = {})
-      new(options).login
+    def headers
+      {
+        'Content-Type' => 'application/x-www-form-urlencoded'
+      }
     end
 
-    def login
-      request(:post, resource_name: user_auth_url)
+    def self.login(steam_id64, password, options = {})
+      new(options).login(steam_id64, password)
+    end
+
+    def login(steam_id64, password)
+      query[:steamid] = steam_id64
+      query[:encrypted_loginkey] = encrypt(password)
+      request(:post, request_options)
     end
 
     private
+
+    def encrypt(password)
+      iv = OpenSSL::Random.random_bytes(16)
+      aes = OpenSSL::Cipher::Cipher.new("AES-256-CBC")
+      aes.encrypt
+      aes.key = session_key
+      aes.iv = iv if iv != nil
+      (aes.update(password) + aes.final).unpack("H*")[0]
+    end
+
+    def crypted_session_key
+      OpenSSL::PKey::RSA.new(certificate, session_key)
+    end
+
+    def certificate
+      File.read certificate_file
+    end
+
+    def request_options
+      {
+        headers: headers,
+        resource_name: user_auth_url,
+        body: query
+      }
+    end
+
+    def default_certificate_file
+      File.expand_path('../../assets/cert.crt', __FILE__)
+    end
 
     def default_user_auth_url
       "https://api.steampowered.com/ISteamUserAuth/AuthenticateUser/v1"
