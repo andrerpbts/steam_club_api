6d920ef5c8079ab2aafe7128f3676347714447f6
andrerpbts <andrerpbts@gmail.com>
2014-12-26 15:17:32 -0200

    Return RSA instance after getting mod and exp from Steam

--- spec/entities/rsa_key_spec.rb
+++ spec/entities/rsa_key_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe SteamClubAPI::Entities::RSAKey do
   let(:rsa_key) { build :rsa_key }
+  let(:expected_rsa_key) { read_fixture('key.pem') }
+
   subject { described_class.new(rsa_key.attributes) }
 
   it { expect(subject.steamid).to eq(rsa_key.steamid) }
   it { expect(subject.publickey_mod).to eq(rsa_key.publickey_mod) }
   it { expect(subject.publickey_exp).to eq(rsa_key.publickey_exp) }
   it { expect(subject.timestamp).to eq(rsa_key.timestamp) }
+
+  it { expect(subject.rsa).to be_a(OpenSSL::PKey::RSA) }
+  it { expect(subject.rsa.to_s).to eq(expected_rsa_key) }
+
 end
